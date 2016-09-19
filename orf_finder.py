51692d834cde3ef2dfbfd2907d5ed50d4e68c8d9
Andrew Natale <anatale@otp589576ots.ucsf.edu>
2016-09-19 16:17:22 -0700

    function to reverse complement

--- orf_finder.py
+++ orf_finder.py
@@ -5,8 +5,17 @@ from seq_functions import *
 script, filename = argv
 
 full_sequence = file_to_sequence(filename)
+reverse_complement = rev_comp_sequence(full_sequence)
 
 forward_frame1 = sequence_to_codons(full_sequence)
 forward_frame2 = sequence_to_codons(full_sequence[1:])
 forward_frame3 = sequence_to_codons(full_sequence[3:])
 
+reverse_frame1 = sequence_to_codons(reverse_complement)
+reverse_frame2 = sequence_to_codons(reverse_complement[1:])
+reverse_frame2 = sequence_to_codons(reverse_complement[2:])
+
+print "forward frame 1:"
+print forward_frame1
+print "reverse frame 1:"
+print reverse_frame1
--- seq_functions.py
+++ seq_functions.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python2
 
 def file_to_sequence(filename):
-	sequence_file = open(filename, 'r')
-	sequence_lines = sequence_file.readlines()
-	sequence_file.close()	
-	# Remove the header
-	sequence_lines.pop(0)
-	# Remove line endings
-	sequence_lines = [i.strip('\r\n') for i in sequence_lines]
-	# Concatenate lines into a single string
-	sequence_str = ''.join(map(str, sequence_lines))
-	return sequence_str
-	
+    sequence_file = open(filename, 'r')
+    sequence_lines = sequence_file.readlines()
+    sequence_file.close()
+    # Remove the header
+    sequence_lines.pop(0)
+    # Remove line endings
+    sequence_lines = [i.strip('\r\n') for i in sequence_lines]
+    # Concatenate lines into a single string
+    sequence_str = ''.join(map(str, sequence_lines))
+    return sequence_str
+
 def sequence_to_codons(sequence_str):
-	codon_list = [ sequence_str[index:index+3] \
-		for index in range(0, len(sequence_str), 3) ]
-	return codon_list
+    codon_list = [ sequence_str[index:index+3] \
+        for index in range(0, len(sequence_str), 3) ]
+    return codon_list
 
 def rev_comp_sequence(sequence_str):
-	pass
+    # Dictionary to define complement bases
+    complement = {'A': 'T',
+                  'T': 'A',
+                  'C': 'G',
+                  'G': 'C'}
+    # Turn sequence string into a list of bases
+    bases = list(sequence_str)
+    # Switch each base in the list with its complement
+    bases = [complement[base] for base in bases]
+    # Join the list back into a single string
+    complement_str = ''.join(bases)
+    # Return the reverse of that string
+    return complement_str[::-1]

24463a59572dfa86e0902b44f0ec74420d32e2d8
Andrew Natale <anatale@otp589576ots.ucsf.edu>
2016-09-20 10:43:30 -0700

    lots of updates

--- orf_finder.py
+++ orf_finder.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python2
 from sys import argv
 from seq_functions import *
-import re
 
 script, filename = argv
 
 forward_sequence = file_to_sequence(filename)
 reverse_complement = rev_comp_sequence(forward_sequence)
 
-for1_orfs = find_orfs(sequence_to_codons(forward_sequence))
-for2_orfs = find_orfs(sequence_to_codons(forward_sequence[1:]))
-for3_orfs = find_orfs(sequence_to_codons(forward_sequence[2:]))
+class ReadingFrame(object):
 
-rev1_orfs = find_orfs(sequence_to_codons(reverse_complement))
-rev2_orfs = find_orfs(sequence_to_codons(reverse_complement[1:]))
-rev3_orfs = find_orfs(sequence_to_codons(reverse_complement[2:]))
+    def __init__(self, input_seq):
+        self.orfs = find_orfs(sequence_to_codons(input_seq))
 
+for1_orfs = ReadingFrame(forward_sequence)
 # make a big list of lists
-all_frames_orfs = [for1_orfs, for2_orfs, for3_orfs,
-                   rev1_orfs, rev2_orfs, rev3_orfs]
+#all_frames_orfs = [for1_orfs, for2_orfs, for3_orfs,
+#                   rev1_orfs, rev2_orfs, rev3_orfs]
 
-for frame in all_frames_orfs:
-    for orf in frame:
-        print orf.indices()
+for orf in for1_orfs.orfs:
+    print 'ORF at ' + str(orf.indices())
+    print 'GC content: ' + str(orf.gc_content())
+    print 'Translation:\n' + orf.translate_orf()
--- seq_functions.py
+++ seq_functions.py
@@ -36,33 +36,76 @@ def rev_comp_sequence(sequence_str):
     # Return the reverse of that string
     return complement_str[::-1]
 
+# Translate a list of codons into a string of amino acids
+def translate(codon_list):
+    codon_to_aa = {'TTT': 'F', 'TTC': 'F', 'TTA': 'L', 'TTG': 'L',
+                   'TCT': 'S', 'TCC': 'S', 'TCA': 'S', 'TCG': 'S',
+                   'TAT': 'Y', 'TAC': 'Y', 'TAA': '*', 'TAG': '*',
+                   'TGT': 'C', 'TGC': 'C', 'TGG': 'W', 'TGA': '*',
+                   'CTT': 'L', 'CTC': 'L', 'CTA': 'L', 'CTG': 'L',
+                   'CCT': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
+                   'CAT': 'H', 'CAC': 'H', 'CAA': 'Q', 'CAG': 'Q',
+                   'CGT': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R',
+                   'ATT': 'I', 'ATC': 'I', 'ATA': 'I', 'ATG': 'M',
+                   'ACT': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',
+                   'AAT': 'N', 'AAC': 'N', 'AAA': 'K', 'AAG': 'K',
+                   'AGT': 'S', 'AGC': 'S', 'AGA': 'R', 'AGG': 'R',
+                   'GTT': 'V', 'GTC': 'V', 'GTA': 'V', 'GTG': 'V',
+                   'GCT': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
+                   'GAT': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E',
+                   'GGT': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G'}
+    aa_list = [codon_to_aa[codon] for codon in codon_list]
+    return ''.join(aa_list)
+
+
+# Class to hold and return information about individual ORFs
 class Orf(object):
+
     def __init__(self, first_index, last_index, codon_list):
         self.first_index = first_index
         self.last_index = last_index
         self.codon_list = codon_list
+        self.sequence_str = ''.join(self.codon_list)
+
     def indices(self):
         return (self.first_index, self.last_index)
+
     def gc_content(self):
-        pass
-    def translate(self):
-        pass
+        self.count_g = self.sequence_str.count('G')
+        self.count_c = self.sequence_str.count('C')
+        self.seq_length = len(self.sequence_str)
+        return (float(self.count_g) + \
+                float(self.count_c)) / \
+                float(self.seq_length)
+
     def codon_bias(self):
         pass
 
+    def translate_orf(self):
+        self.aa_seq = translate(self.codon_list)
+        return self.aa_seq
+
+# Find ORFs in a list of codons and turn them into a list of Orf objects
 def find_orfs(codon_list):
+    # Regex to identify start and stop codons
     start_codon = re.compile('ATG')
     stop_codons = re.compile('TAA|TAG|TGA')
     orf_list = []
+    # The 'started' variable is a switch to identify when the functions
+    # is reading through an ORF
     started = False
     for index, codon in enumerate(codon_list):
+        # When a start codon is found, start recording codons
         if not started and start_codon.match(codon):
             started = True
             tmp_list = []
             first_index = index
             tmp_list.append(codon)
+        # Record all the codons after the start codon
         elif started and not stop_codons.match(codon):
             tmp_list.append(codon)
+        # When a stop codon is found, stop recording codons,
+        # build an Orf object, and add it to the list.
         elif started and stop_codons.match(codon):
             started = False
             tmp_list.append(codon)
